bd61b2a0440ddfa9aadb687a6e96e68232faaeb5
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-01-19 11:04:54 +0200

    last commit

--- store/modules/basket/actions.js
+++ store/modules/basket/actions.js
@@ -3,7 +3,7 @@ import { ADD_PRODUCT, CHANGE_QUANTITY, REMOVE_PRODUCT } from './mutation-types'
 export default {
   addToBasket({ commit }, product) {
     product.quantity = 1
-    commit(ADD_PRODUCT, product)
+    commit(x, product)
   },
   removeFromBasket({ commit }, productId) {
     commit(REMOVE_PRODUCT, productId)
